7ee6cb5c55dca5580ae3ed067d9d4c90c471365e
sergej2 <menshov-sergej@yandex.ru>
2019-04-15 11:45:05 +0300

    Add run on test data

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import math
 
 
 def get_rotate_angle(src_pts, dst_pts):
-
-    #Получаем матрицу афинного преобразования, размерность 2*3
+    # Получаем матрицу афинного преобразования, размерность 2*3
     # Элемент a21 = -scale * cos(alpha)
     # Элемент a21 = scale * sin(alpha)
     # Чтобы найти alpha нужно взять arctan(-a21 / a22)
@@ -17,11 +16,10 @@ def get_rotate_angle(src_pts, dst_pts):
     if a22 == 0:
         alpha = 90,
     else:
-        #Возвращает значение в радианах
+        # Возвращает значение в радианах
         alpha = math.atan(-a21 / a22)
         alpha = math.degrees(alpha)
 
-    print("rotate angle = "+str(alpha))
     return alpha
 
 
@@ -57,26 +55,28 @@ def read_data(name_file):
 def generate_test(src, center_src):
     test_alpha = 30
 
-    #Получаем матрицу поворота на заданный угол
+    # Получаем матрицу поворота на заданный угол
     rot_mat = cv.getRotationMatrix2D(center_src, test_alpha, 1)
-    #Добавляем к матрице поворота вектор (0,0,1) для того чтобы применить перспективное преобразование
+    # Добавляем к матрице поворота вектор (0,0,1) для того чтобы применить перспективное преобразование
     rot_mat = np.vstack((rot_mat, [0, 0, 1]))
-    #Получаем точки после поворота
+    # Получаем точки после поворота
     dst = cv.perspectiveTransform(src, rot_mat)
 
-    #Делаем растяжение по осям и сдвиг
+    # Делаем растяжение по осям и сдвиг
     for i in range(3):
         dst[0][i][0] *= 10 - 100
         dst[0][i][1] *= 20 + 6
 
     alpha = get_rotate_angle(src, dst)
-
+    print("test angle = " + str(alpha))
     assert abs(test_alpha - alpha) < 0.1
 
 
 if __name__ == '__main__':
-    #Чтение данных
-    src, center_src, dst, center = read_data("test")
+    # Чтение данных
+    src, center_src, dst, center = read_data("test.txt")
     # Генерируем тест, к исходным точкам применяем поворот+растяжение по осям и сдвиг
     # После этого сравниваем ответы
     generate_test(src, center_src)
+    angle = get_rotate_angle(src, dst)
+    print("rotate angle = " + str(angle))
